f004fdf5e96d6546638b6237beee3ebcb56ef5f5
anaungurean <anamariaungurean01@gmail.com>
2023-10-17 17:36:50 +0300

    Update Ex3.py

--- Laborator_01/Ex3.py
+++ Laborator_01/Ex3.py
@@ -5,12 +5,16 @@ second_string = input("Type your second string: ")
 number_of_occurrences = 0
 position = 0
 
-while  first_string in second_string[position:]:
-    number_of_occurrences += 1
-    position = second_string.index(first_string, position) + len(first_string)
+while True:
+    found_position = second_string.find(first_string, position)
+    if found_position != -1:
+        number_of_occurrences += 1
+        position = found_position + 1
+    else:
+        break
 
-#varianta mai simpla
-# number_of_occurrences= second_string.count(first_string)
 
-print("Number of occurrences of the first string in the second: ",number_of_occurrences)
+
+print("Number of occurrences of the first string in the second:", number_of_occurrences)
+
 
